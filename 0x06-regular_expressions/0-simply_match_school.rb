4bb63059ee807dfdf4c5163604729b67475511e1
AchrafEs <achraf.es2110@gmail.com>
2023-08-03 14:19:55 +0000

    updated the file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,4 @@
 #!/usr/bin/env ruby
+#Simply matching School
 
-def match_school(text)
-  regex = /School/
-  match = text.match(regex)
-  puts match ? match[0] : ''
-end
-
-if __FILE__ == $0
-  if ARGV.empty?
-    puts "Usage: #{$0} <text>"
-  else
-    match_school(ARGV[0])
-  end
-end
+puts ARGV[0].scan(/[S]chool/).join
