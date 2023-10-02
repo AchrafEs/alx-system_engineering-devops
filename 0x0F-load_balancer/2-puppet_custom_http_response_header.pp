6d52e48018e249a183a7b4a6daa906ce179d8b3c
AchrafEs <achraf.es2110@gmail.com>
2023-10-02 17:34:56 +0000

    updated the puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,42 +1,41 @@
-file { '/var/www/html':
-  ensure => directory,
+# 2-puppet_custom_http_response_header.pp
+
+# Install Nginx
+package { 'nginx':
+  ensure => 'installed',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
-}
-
-file { '/var/www/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page",
-}
-
+# Configure Nginx with custom HTTP header
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
-  content => '
-    server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-      add_header X-Served-By $hostname;
-      root /var/www/html;
-      index index.html index.htm;
-
-      location /redirect_me {
-        return 301 https://youtube.com/;
-      }
-
-      error_page 404 /404.html;
-      location /404.html {
-        root /var/www/html;
-        internal;
-      }
-    }
-  ',
+  content => template('your_module/nginx_config.erb'), # You need to create a template file for Nginx configuration
+  notify  => Service['nginx'],
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+# Enable the default site (symbolic link)
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
   require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
+
+# Create an Nginx custom configuration template
+file { '/etc/puppet/modules/your_module/templates/nginx_config.erb':
+  ensure  => present,
+  content => "# Nginx custom configuration\n
+              server {\n
+                listen 80 default_server;\n
+                listen [::]:80 default_server;\n
+                add_header X-Served-By $::hostname;\n
+                root /var/www/html;\n
+                index index.html index.htm;\n
+              }",
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/nginx/sites-enabled/default'],
 }
