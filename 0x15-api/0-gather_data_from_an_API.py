e5bc070e14bc6da98b647af6ff9fc1cdd3d46a6b
AchrafEs <achraf.es2110@gmail.com>
2023-10-25 14:19:35 +0000

    updated the script

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,66 +6,29 @@ This script accepts an integer employee ID as a command-line
 argument and displays the employee's TODO list progress.
 
 Usage:
-python3 my-script.py ID
-
-Requirements:
-- You must use urllib or requests module
-- The script must accept an integer as a parameter, which
-  is the employee ID
-- The script must display on the standard output the employee
-  TODO list progress in a specific format
-- First line: Employee EMPLOYEE_NAME is done with
-  tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS)
-- EMPLOYEE_NAME: name of the employee
-- NUMBER_OF_DONE_TASKS: number of completed tasks
-- TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum
-  of completed and non-completed tasks
-- Second and N next lines display the title of completed tasks:
-  TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+python3 my-script.py id
 """
 
 import requests
 import sys
 
-
-def get_employee_todo_progress(employee_id):
-    """Get and display an employee's TODO list progress."""
-    # Make an HTTP GET request to the JSONPlaceholder API
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        # Parse the JSON response
-        todos = response.json()
-
-        # Count the number of completed tasks
-        completed_tasks = [todo for todo in todos if todo['completed']]
-        completed = len(completed_tasks)
-        total = len(todos)
-
-        # Get the employee name
-        user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        user_info = requests.get(user_url).json()
-        name = user_info.get('name', 'Unknown Employee')
-
-        # Display the employee's TODO list progress
-        pm = f"Employee {name} is done with tasks ({completed}/{total}):"
-        print(pm)
-        # Display the titles of completed tasks
-        for task in completed_tasks:
-            print(f"\t{task['title']}")
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 my-script.py ID")
-        sys.exit(1)
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    try:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-    except ValueError:
-        print("Invalid employee ID. Please provide a valid integer.")
-        sys.exit(1)
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    params = {"userId": employee_id}
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
+    completed = []
+
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
+
+    print("Employee {} is done with tasks ({}/{})"
+          .format(user.get("name"), len(completed), len(todos)))
+
+    for complete in completed:
+        print("\t{}".format(complete))
