b94825a7a6aee0b13aa2ec987054a96dad7812dd
AchrafEs <achraf.es2110@gmail.com>
2023-11-16 13:58:10 +0000

    Updated the script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,10 +3,12 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/usr/local/bin', '/bin'],  # Use an array for the path
+  onlyif  => 'test $(ulimit -n) -lt 4096',  # Check if change is needed
 } ->
 
 exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command => 'service nginx restart',  # Use 'service' to restart Nginx
+  path    => '/usr/sbin/:/usr/bin/:/sbin/:/bin/',  # Add necessary paths
+  require => Exec['fix--for-nginx'],  # Make sure the first exec runs first
 }
