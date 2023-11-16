80f7356b4d6be75cd514d9ec3e739ca0e6ce0cea
AchrafEs <achraf.es2110@gmail.com>
2023-11-16 14:08:13 +0000

    fixed puppet-linter issue

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,9 +5,7 @@ exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => ['/usr/local/bin', '/bin'],  # Use an array for the path
   onlyif  => 'test $(ulimit -n) -lt 4096',  # Check if change is needed
-} ->
-
-exec { 'nginx-restart':
+} -> exec { 'nginx-restart':
   command => 'service nginx restart',  # Use 'service' to restart Nginx
   path    => '/usr/sbin/:/usr/bin/:/sbin/:/bin/',  # Add necessary paths
   require => Exec['fix--for-nginx'],  # Make sure the first exec runs first
